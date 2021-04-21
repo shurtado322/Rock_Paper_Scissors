6581570970b6559c5bed8a8d1564378a1f28d5c3
shurtado322 <sergiohurtado322@gmail.com>
2021-04-20 18:45:57 -0700

    Implemented computer to randomize a choice between rock paper scissors. Added a comparison between the computer's choice and the user's choice to determine the winner or if the game is a tie.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,6 +4,13 @@ console.log("Welcome to ROCK, PAPER, SCISSORS!\n\n");
 let usrChoice;          // Store user's selection to compare with computer selection
 let cpuChoice;          // Store computer's selection.
 let isValid = false;    // Input validation boolean for user choice
+const MAX = 3;          // Number of options for computer to randomize
+
+// Randomization function that will return an integer between 0 and input number
+function getRandInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 // Do while loop will persist until a valid choice is entered by the user
 do {
     // Get user choice
@@ -24,4 +31,35 @@ do {
     }
 }
 while (isValid == false);
+
+// Generate a random choice for computer player to use
+cpuChoice = getRandInt(MAX);
+// Assign cpu choice to a each value
+if (cpuChoice == 0) {
+    cpuChoice = "rock";
+}
+else if (cpuChoice == 1) {
+    cpuChoice = "paper";
+}
+else {
+    cpuChoice = "scissors";
+}
+// Display both choices to user
+console.log("\nYou chose: " + usrChoice);
+console.log("\nComputer chose: " + cpuChoice);
+// Go through each possibility and find out who wins
+// If computer choice and user choice are the same, the game is a tie
+if (cpuChoice == usrChoice) {
+    console.log("\nIT's A TIE!!!\n");
+}
+// List of cases in which computer would win
+else if ((cpuChoice == "scissors" && usrChoice == "paper") || 
+         (cpuChoice == "paper" && usrChoice == "rock") || 
+         (cpuChoice == "rock" && usrChoice == "scissors")) {
+    console.log("\nComputer Wins!!!\n");
+}
+// If computer does not win and game is not a tie, user wins
+else {
+    console.log("\nYOU WIN!!!\n");
+}
 
